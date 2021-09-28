ce57df522dcf921ccb603fbb93e2e4b75b79ccd1
Francis Gonzales <fgonzalestello91@gmail.com>
2021-09-28 15:16:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -88,8 +88,14 @@ if (($file = fopen(FILE_PATH, 'r')) !== false) {
             'password' => $password,
             'activo' => true
         ];
-        $createdUsers[] = createUser($token->access_token, $fields);
+        $response = $createdUsers[] = createUser($token->access_token, $fields);
         $r++;
+        
+        if ($response->errorStatus == 'ERROR') {
+            error_log("User not created, error: " . $response->exceptionClass . ' data:'  . json_encode($fields), 3, __DIR__ . '/creation_error.log');
+        } else {
+           error_log("User created correctly: " . json_encode($fields), 3, __DIR__ . '/creation_success.log');
+        }
     }
     fclose($file);
 
